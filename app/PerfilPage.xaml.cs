54f49a132f8063e8cd7b328c0c1dee20ca844be8
manuelcpedrero <manuelcpedrero@gmail.com>
2026-04-19 18:08:24 +0200

    Modo Oscuro MainPage hecho

--- app/PerfilPage.xaml.cs
+++ app/PerfilPage.xaml.cs
@@ -8,6 +8,29 @@ namespace app
         public PerfilPage()
         {
             InitializeComponent();
+
+            // Al abrir la página, comprobamos si el modo oscuro ya está activo
+            // para poner el Switch en la posición correcta
+            if (Application.Current.UserAppTheme == AppTheme.Dark)
+            {
+                SwitchModoOscuro.IsToggled = true;
+                LblEstadoModoOscuro.Text = "Activado";
+            }
+        }
+
+        // Este es el nuevo evento que se ejecuta al tocar el Switch
+        private void OnModoOscuroToggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value) // Si el switch se enciende (true)
+            {
+                Application.Current.UserAppTheme = AppTheme.Dark;
+                LblEstadoModoOscuro.Text = "Activado";
+            }
+            else // Si el switch se apaga (false)
+            {
+                Application.Current.UserAppTheme = AppTheme.Light;
+                LblEstadoModoOscuro.Text = "Desactivado";
+            }
         }
 
         private async void OnBackClicked(object sender, EventArgs e)
